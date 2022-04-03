48396d7ef976a7617dfa5f80e9fbe75c4a84ee75
Josh Willox <joshwillox@gmail.com>
2022-04-03 21:00:58 +1000

    Fix async import

--- upbankapi/__init__.py
+++ upbankapi/__init__.py
@@ -1,3 +1,8 @@
 """Typed python client for interacting with Up's banking API."""
-from .client import Client, AsyncClient
+from .client import Client
 from .exceptions import *
+
+try:
+    from .client import AsyncClient
+except ImportError:
+    pass
--- upbankapi/client/__init__.py
+++ upbankapi/client/__init__.py
@@ -1,2 +1,6 @@
-from ._async import AsyncClient
 from ._sync import Client
+
+try:
+    from ._async import AsyncClient as AsyncClient
+except ValueError:
+    pass
