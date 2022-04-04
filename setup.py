04cbf3569de2a6171edd86e983e35b8a1fb3d78f
Josh Willox <joshwillox@gmail.com>
2022-04-04 17:36:20 +1000

    Update build files and requirements

--- setup.py
+++ setup.py
@@ -1,31 +1,44 @@
 from setuptools import find_packages, setup
 
-PACKAGE_NAME = "up-bank-api"
-VERSION = "0.3.2"
-PROJECT_URL = "https://github.com/jcwillox/up-bank-api"
-PROJECT_AUTHOR = "Joshua Cowie-Willox"
-DOWNLOAD_URL = f"{PROJECT_URL}/archive/{VERSION}.zip"
-PACKAGES = find_packages()
+PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
-with open("README.md", "r", encoding="UTF-8") as file:
-    LONG_DESCRIPTION = file.read()
+REQUIRES = [
+    "requests>=2.22.0,<3",
+]
+
+DOCS_REQUIRE = [
+    "mkdocs-material>=8.2.8,<9",
+    "mkdocstrings>=0.18.1",
+]
+
+TESTS_REQUIRE = [
+    "pytest>=7,<8",
+    "requests_mock>=1.8.0,<2",
+]
+
+ASYNC_REQUIRE = [
+    "aiohttp>=3.7.4,<4",
+]
+
+EXTRAS_REQUIRE = {
+    "docs": DOCS_REQUIRE,
+    "tests": TESTS_REQUIRE,
+    "async": ASYNC_REQUIRE,
+}
+
 
 if __name__ == "__main__":
     setup(
-        name=PACKAGE_NAME,
-        version=VERSION,
-        url=PROJECT_URL,
-        download_url=DOWNLOAD_URL,
-        author=PROJECT_AUTHOR,
-        author_email="",
         packages=PACKAGES,
-        long_description=LONG_DESCRIPTION,
-        long_description_content_type="text/markdown",
+        install_requires=REQUIRES,
+        tests_require=TESTS_REQUIRE,
+        extras_require=EXTRAS_REQUIRE,
         python_requires=">=3.7",
-        install_requires=["requests>=2.14.0"],
         classifiers=[
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
